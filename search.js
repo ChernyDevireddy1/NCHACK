75be3263469da26d92f8f4c15663ea507afb3ea6
smahavadi <svmahavadi@gmail.com>
2023-04-08 18:11:13 -0400

    working to input csv file

--- search.js
+++ search.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-    var data = $.csv.toObjects(csv);
-    console.log("data:", data)
-    
+    csv = "/Users/SumaVM/Documents/NCHACK/files/Metro_invt_fs_uc_sfrcondo_sm_month.csv";
+    // var data = $.csv.toArrays(csv);
+    // console.log("data:", data)
+    myForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const input = csvFile.files[0];
+        const reader = new FileReader();
+        reader.onload = function (e) {
+           const text = e.target.result;
+           document.write(text);
+        };
+        reader.readAsText(input);
+     });
 });
