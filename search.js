bf9ae50177b367b5500f01606cbd62e4c8c03ca8
smahavadi <svmahavadi@gmail.com>
2023-04-08 20:41:10 -0400

    trying to input csv pt 2

--- search.js
+++ search.js
@@ -1,19 +1,54 @@
 $(document).ready(function() {
-    var csv = "../downloads/Metro_invt_fs_uc_sfrcondo_sm_month";
+  const filename = "http://github.com/ChernyDevireddy1/NCHACK/blob/main/files/Metro_invt_fs_uc_sfrcondo_sm_month.csv"; // replace with your desired filename
+
+  var data;
+  console.log("hi");
+  var xhr = new XMLHttpRequest();
+  console.log("output");
+  xhr.open('GET', filename);
+  xhr.onload = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          // Parse CSV data
+          const parsedData = Papa.parse(xhr.responseText, { header: true });
+          
+          console.log(parsedData.data); // Access parsed data as an array of objects }
+      }
+  };
+  xhr.send(null);
+    // data = $.csv.toArrays(xhr.responseText);
+    // console.log(data);
+//   fetch(csv)
+//     .then(response => response.text())
+//     .then(data => {
+//       const csvData = [];
+//       csv(data)
+//         .on('data', row => {
+//           csvData.push(row);
+//         })
+//         .on('end', () => {
+//           console.log(csvData);
+//           // do something with csvData
+//         });
+//     })
+    // .catch(error => console.error(error));
+
+
     // var data = $.csv.toArrays(csv);
-    // console.log("data:", data)
-    var myForm = document.getElementById("form"); // Added missing element with ID "form"
+    // console.log("check",csv.file[0]);
+    // console.log("data:", data);
 
-    myForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const input = csvFile.files[0];
-       // const input = myForm.querySelector('input[type="file"]').files[0]; // Replaced "csvFile" with "myForm.querySelector('input[type="file"]')"
+    // myForm.addEventListener("submit", function (e) {
+    //     e.preventDefault();
+    //     console.log("button has been clicked");
+    //     const input = csv.querySelector('input[type="file"]').files[0];//csvFile.files[0];
+    //    // const input = myForm.querySelector('input[type="file"]').files[0]; // Replaced "csvFile" with "myForm.querySelector('input[type="file"]')"
 
-        const reader = new FileReader();
-        reader.onload = function (e) {
-           const text = e.target.result;
-           document.write(text);
-        };
-        reader.readAsText(input);
-     });
+    //     const reader = new FileReader();
+    //     reader.onload = function (e) {
+    //        const text = e.target.result;
+    //        document.write(text);
+    //        console.log(text);
+    //     };
+    //     reader.readAsText(input);
+    //  });
 });
