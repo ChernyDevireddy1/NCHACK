c486a0d24316f1d0e8c56ef0a78b83a31af2e461
smahavadi <svmahavadi@gmail.com>
2023-04-09 09:57:17 -0400

    added pricing list

--- rethinkRE/src/app/search/search.component.ts
+++ rethinkRE/src/app/search/search.component.ts
@@ -16,15 +16,34 @@ export class SearchComponent implements OnInit{
 
   public propMaps = "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d104190.59469267109!2d-80.80520393999278!3d35.307072669417366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1shouses%20for%20sale%20in%20charlotte%20nc!5e0!3m2!1sen!2sus!4v1681021769650!5m2!1sen!2sus";
 
+  public property = "Residential";
+
+  public rates = [387231, 399270, 294762, 325521, 395658, 395709];
+
   ngOnInit(): void {
     // let gMap = document.getElementById("map");
     // gMap?.setAttribute("src", this.ogMaps)
   }
 
-  getMap(){
+  changeProp(value:string): void {
+    this.property = value;
+  }
+
+  getMap() {
     let gMap = document.getElementById("map");
     console.log(gMap);
-    gMap?.setAttribute("src", this.propMaps)
+    gMap?.setAttribute("src", this.propMaps);
+    let pricing = document.getElementById("prices");
+    while(pricing?.firstChild){
+      pricing.removeChild(pricing.firstChild);
+  }
+    for (let i = 0; i < 5; i++) {
+      var text = document.createTextNode("Pricing for " + this.property + " #" + (i + 1) + " is $" + this.rates[i]);
+      pricing?.appendChild(text);
+      var br = document.createElement("br");
+      pricing?.appendChild(br);
+    }
+
   }
 
 
